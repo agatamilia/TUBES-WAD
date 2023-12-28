5eb730b4e24c3f007a13c32ac36108aabef44d19
fdzra <fadhilahazzahra@student.telkomuniversity.ac.id>
2023-12-28 20:31:49 +0700

    index bre

--- tubes-wad/routes/web.php
+++ tubes-wad/routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/index', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/counsoler', function () {
+    return view('counsoler');
+})->name('counsoler');
+
+Route::get('/service', function () {
+    return view('service');
+})->name('service');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/appointment', function () {
+    return view('appointment');
+})->name('appointment');
